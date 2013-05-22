4479d483ca80a13959deef8f0e09e0c2c8eeb3c1
kenn <kennmunene@gmail.com>
2013-05-22 15:39:17 +0300

    complete first time

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ def create
     render :text => request.env['omniauth.auth'].inspect
     auth = request.env['omniauth.auth']
     unless @auth = Authorization.find_from_hash(auth)
-    @auth = Authorization.create_from_has(auth,current_user)
+    @auth = Authorization.create_from_hash(auth,current_user)
     end
     self.current_user = @auth.user
     render :text => "Welcome #{current_user.name}"
