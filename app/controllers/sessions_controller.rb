1394c8e9d7595d8775d94f96afcf4375db8bd353
kenn <kennmunene@gmail.com>
2013-05-22 14:22:59 +0300

    signup start

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,5 @@
 class SessionsController < ApplicationController
-def create
-  
-    render :text => request.env['rack.auth'].inspect
+def create  
+    render :text => request.env['omniauth.auth'].inspect
   end
 end
