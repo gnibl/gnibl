f3ac67aafcf39257a54e82b9af64f89d483d3f26
marembo <gniblteam@gmail.com>
2013-06-02 10:24:19 +0300

    key config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,10 +4,10 @@ config.root = "#{Rails.root}/tmp"
 config.cache_dir = "carrierwave"
 
  config.fog_credentials = {
-    :provider               => 'AWS',                        # required
-    :aws_access_key_id      => 'AKIAINZNZNMGHHBCEBPQ',                        # required
-    :aws_secret_access_key  => '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',                        # required
-    :region                 => 'eu-west-1',                  # optional, defaults to 'us-east-1'
+    :provider               => 'TRW',                        # required
+    :aws_access_key_id      => 'sajdhasas8u8923nfkcljjauiruwcnjf',                        # required
+    :aws_secret_access_key  => 'dfhsdkjfhwierlsdfiofnjsdbfsdl;njk',                        # required
+    :region                 => '7236jksfbjsd',                  # optional, defaults to 'us-east-1'
   # :host                   => 's3-website-eu-west-1.amazonaws.com',             # optional, defaults to nil
  #   :endpoint               => '' # optional, defaults to nil. will screw me big time
   }
