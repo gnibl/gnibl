8d55cc53e613d3d9635b7166003c2ccd779ec646
Marembo Ochieng <marembo2008@gmail.com>
2013-06-25 15:48:11 +0300

    marembos commit

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -55,7 +54,7 @@ ActiveRecord::Schema.define(:version => 20130625081521) do
     t.string   "image"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
-    t.integer  "city"
+    t.string   "city"
   end
 
   add_index "gnibs", ["user_id", "created_at"], :name => "index_gnibs_on_user_id_and_created_at"
@@ -77,10 +76,10 @@ ActiveRecord::Schema.define(:version => 20130625081521) do
     t.datetime "updated_at",      :null => false
     t.string   "password_digest"
     t.string   "email"
+    t.string   "city"
     t.string   "avatar"
     t.string   "description"
     t.string   "username"
-    t.integer  "city"
     t.string   "surname"
     t.datetime "birthday"
     t.integer  "city_id"
