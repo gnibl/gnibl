e77d7139134ac5896a1e33a5566af4f16ea0fcd7
kenn <kennmunene@gmail.com>
2013-06-25 16:41:17 +0300

    hash tag support

--- app/models/gnib.rb
+++ app/models/gnib.rb
@@ -17,24 +17,14 @@ class Gnib < ActiveRecord::Base
 
   def parsed_description
 comment = self.description
-pos = -1
 final_comment = ""
 len = comment.length
-lastpos = 0
-while (pos = comment.index('@',pos+1))
- tag =  comment[pos+1 ..len].split(" ")[0]
- replacement_string = "<a style='color: #17aeff' href = '/users/search?uid ="+tag+"'>"+tag+"</a>"
- final_comment += comment[lastpos..pos] + replacement_string
-lastpos = pos +1+tag.length
-if t = tag.index('@',0) # cater for @address@gmail.com
-pos = comment.index('@',pos+1)
-end
-end
+
 lastpos = 0
 pos = -1
 while (pos = comment.index('#',pos+1))
  tag =  comment[pos+1 ..len].split(" ")[0]
- replacement_string = "<a style='color: #17aeff' href = '/gnibs/search?uid ="+tag+"'>"+tag+"</a>"
+ replacement_string = "<a style='color: #17aeff' href = '/gnibs/search?term="+tag+"'>"+tag+"</a>"
  final_comment += comment[lastpos..pos] + replacement_string
 lastpos = pos +1+tag.length
 if t = tag.index('@',0) # cater for @address@gmail.com
@@ -42,24 +32,17 @@ pos = comment.index('@',pos+1)
 end
 end
 
+pos = -1
+lastpos = 0
+while (pos = comment.index('@',pos+1))
+ tag =  comment[pos+1 ..len].split(" ")[0]
+ replacement_string = "<a style='color: #17aeff' href = '/users/search?uid="+tag+"'>"+tag+"</a>"
+ final_comment += comment[lastpos..pos] + replacement_string
+lastpos = pos +1+tag.length
+if t = tag.index('@',0) # cater for @address@gmail.com
+pos = comment.index('@',pos+1)
+end
+end
  return final_comment.html_safe
-#    term = self.description[/#[\w\d]+\b/i]
-#    if term
-#      term = term[1..-1]
-#    end
-#    uid = self.description[/\@[\w\d]+\b/i]
-#    if uid
-#      uid = uid[1..-1]
-#    end
-#    puts "terms and uid #{term} #{uid}"
-#    parsed_description = self.description
-#    if(term)
-#      parsed_description = parsed_description.gsub(/#[\w\d]+\b/i, '<a href="/gnibs/search?term='+term+'" style="color: #17aeff">\0</a>')
-#    end
-#    if uid
- #     parsed_description = parsed_description.gsub(/\@[\w\d]+\b/i, '<a href="/users/search?uid='+uid+'" style="color: #17aeff">\0</a>')
-#    end
-#    puts "parsed_description #{parsed_description}"
-#    parsed_description.html_safe
   end
 end
